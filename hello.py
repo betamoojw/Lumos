2b33a56411ba862e445c2bd8d3f4147eda2fd47b
jonathanrjpereira <jonathanrjpereira@gmail.com>
2016-09-13 21:18:42 +0530

    Added the test.py code to hello.py code

--- hello.py
+++ hello.py
@@ -1,19 +1,9 @@
 from datetime import datetime
 from datetime import date
-from __future__ import division
-from operator import add
 
-
-blank_day=[0]*24
-
-avg_mon=[0]*24
+blank_mon=[0]*24
+sum_mon=blank_mon
 count_mon=0
-sum_mon=[0]*24
-z_mon=[0]*25
-mon_count=0
-avg_num_mon=[]
-
-
 
 #Gets the Initial Week of the Year since Power On
 while True:
@@ -31,8 +21,9 @@ current_week= datetime.now().isocalendar()[1]
 #Operations carried out during Intital Learning Phase
 while current_week<ending_week:
 
-    if(datetime.now().hour==0): #Starts at 0AM in the Morning
+    if(datetime.now().hour==22): #Starts at 10PM in the Morning
         #Include 7 hour LED Colour Change
+    if(datetime.now().hour==6):  #Wake Up Lighting sequence at 6AM in the Morning
 
     while(datetime.now().isoweekday()==1):   #Monday
         while(#sensor readings true):
@@ -43,50 +34,36 @@ while current_week<ending_week:
         if(datetime.now().isoweekday()!=1):   #Not Monday
             curr_mon=eval(curr_mon)
             count_mon=count_mon+1
-            sum_mon=map(add,sum_mon,curr_mon)
-            avg_mon = map(lambda w_mon: float(w_mon)/count_mon, sum_mon) #Average of All Mondays.
-            list_mon=avg_mon + avg_mon[0:6] # Average 24hour + Average another 6 hour
-            x_mon=[list_mon[i_mon:i_mon+7] for i_mon in xrange(0,len(list_mon), 1)] #list having sublists of various combinations
-            for sub_mon in x_mon:    #List containing only 7 hour sublists
-                if len(sub_mon)==7:
-                    y_mon=sub_mon
-                    mon_count+=1
-                    z_mon[mon_count]=y_mon
-            b_mon=z_mon[17:24]+z_mon[1:2]  #List containing sleeping period sublists
-            for c_mon in b_mon:
-                avg_num_mon+=[sum(c_mon[0:])/len(c_mon[0:])]   #Average of each sleeping period sublists
-            max_val_mon = max(avg_num_mon)
-            max_index_mon = avg_num.index(max_val_mon) #Index of the Maximum Averaged Sleeping Period Sublist. This index determines start of the Sleep Cycle Lighting schedule
+            print "Monday Count:",count_mon
+            sum_mon=map(add,sum_mon,curr_mon)                   #Adds all the Mondays together for each hour
+            print "Total sum of all Mondays::",sum_mon
+            avg_mon = map(lambda w_mon: float(w_mon)/count_mon, sum_mon)    #Gets the average of the Mondays for each hour
+            print "Average Monday::", avg_mon
+            list_mon=avg_mon + avg_mon[0:6]
+            print "30 hour Monday::",list_mon
+
+
+            x_mon=filter(lambda x: len(x)==7,[list_mon[i_mon:i_mon+7] for i_mon in xrange(0,len(list_mon), 1)])
+            print "7 hour MOnday",x_mon
+            print "Length x_mon",len(x_mon)
+
+            z_mon=x_mon[17:24]+x_mon[1:3]     #Sleeping Period from 5pm to 2am
+            print "Sleeping period",z_mon
+            print "Length z_mon",len(z_mon)
+
+            avg_mon=map(lambda a_mon:sum(a_mon)/7,z_mon)    #Takes Average of Each 7-hour-sleeping Period
+            print "Average Mondays",avg_mon
+
+
+            max_mon=max(avg_mon)                            #Which Sleeping Period is used the Maximum
+            print "Maximum Monday",max_mon
+
+
+            best_index=max(enumerate(avg_mon), key=itemgetter(1))[0]
+            print best_index
             break
+    if (current_week>=ending_week):
+        break
 
 
 while current_week>=ending_week:
-
-    while(datetime.now().isoweekday()==1):   #Monday
-
-        if(datetime.now().hour)
-
-
-        while(#sensor readings true):
-            curr_mon= blank_day
-            curr_mon[datetime.now().hour]=1
-            if(#sensor is false):
-                break
-        if(datetime.now().isoweekday()!=1):   #Not Monday
-            curr_mon=eval(curr_mon)
-            count_mon=count_mon+1
-            sum_mon=map(add,sum_mon,curr_mon)
-            avg_mon = map(lambda w_mon: float(w_mon)/count_mon, sum_mon) #Average of All Mondays.
-            list_mon=avg_mon + avg_mon[0:6] # Average 24hour + Average another 6 hour
-            x_mon=[list_mon[i_mon:i_mon+7] for i_mon in xrange(0,len(list_mon), 1)] #list having sublists of various combinations
-            for sub_mon in x_mon:    #List containing only 7 hour sublists
-                if len(sub_mon)==7:
-                    y_mon=sub_mon
-                    mon_count+=1
-                    z_mon[mon_count]=y_mon
-            b_mon=z_mon[17:24]+z_mon[1:2]  #List containing sleeping period sublists
-            for c_mon in b_mon:
-                avg_num_mon+=[sum(c_mon[0:])/len(c_mon[0:])]   #Average of each sleeping period sublists
-            max_val_mon = max(avg_num_mon)
-            max_index_mon = avg_num.index(max_val_mon) #Index of the Maximum Averaged Sleeping Period Sublist. This index determines start of the Sleep Cycle Lighting schedule
-            break
